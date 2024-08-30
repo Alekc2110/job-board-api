4c9fbedba28b2f5077e40482db3e22b79201adbd
Aleksandr Korshunovych <korsunovica@gmail.com>
2024-08-30 17:17:45 +0300

    refactored pom.xml, application,yml

--- src/main/java/my/test/faceit/JobBoardApiApplication.java
+++ src/main/java/my/test/faceit/JobBoardApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class JobBoardApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(JobBoardApiApplication.class, args);
 	}
 
 }
